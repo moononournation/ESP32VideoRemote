d63beeb82cc5e222111f0ae2190dcbf86eeadf1c
moononournation <moononournation@gmail.com>
2019-09-06 12:30:42 +0800

    add ESP32_CAM pins

--- ESP32CameraRobot/camera_pins.h
+++ ESP32CameraRobot/camera_pins.h
@@ -37,6 +37,32 @@
 #define HREF_GPIO_NUM    27
 #define PCLK_GPIO_NUM    25
 
+#elif defined(CAMERA_MODEL_ESP32_CAM)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM       5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#define SD_CLK            14
+#define SD_CMD            15
+#define SD_SDA            2
+#define SD_SDA1           4
+#define SD_SDA2           12
+#define SD_SDA3           13
+
 #elif defined(CAMERA_MODEL_M5CAM)
 #define PWDN_GPIO_NUM     -1
 #define RESET_GPIO_NUM    15
@@ -56,7 +82,7 @@
 #define HREF_GPIO_NUM     26
 #define PCLK_GPIO_NUM     21
 
-#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
+#elif defined(CAMERA_MODEL_M5CAM_PSRAM)
 #define PWDN_GPIO_NUM     -1
 #define RESET_GPIO_NUM    15
 #define XCLK_GPIO_NUM     27
@@ -75,7 +101,7 @@
 #define HREF_GPIO_NUM     26
 #define PCLK_GPIO_NUM     21
 
-#elif defined(CAMERA_MODEL_M5STACK_WIDE)
+#elif defined(CAMERA_MODEL_M5CAM_PSRAM_WIDE)
 #define PWDN_GPIO_NUM     -1
 #define RESET_GPIO_NUM    15
 #define XCLK_GPIO_NUM     27
